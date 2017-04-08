0ab3b256d812d3107f13a404dbb5185fac17db9e
Frismos <Hrachya@mb-3.local>
2017-04-08 19:14:40 +0400

    no message

---
+++ Assets/Scripts/Manager/DialogManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+
+	public GameObject gameOverPanel;
+
+	public static DialogManager instance;
+
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (this);
+		}
+	}
+
+	public void ShowGameOver(){
+		gameOverPanel.SetActive (true);
+	}
+
+	public void HideGameOver(){
+		gameOverPanel.SetActive (false);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
