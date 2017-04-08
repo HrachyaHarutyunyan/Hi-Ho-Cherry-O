d07d753c710765bac01465da51919579749cb9bd
Edgar <edgar@Edgars-MacBook-Pro.local>
2017-04-08 18:09:50 +0400

    no message

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -19,6 +19,56 @@ public class Player : PlayerBehaviour {
 	}
 
 	[PunRPC]
+	private void RouletteSpinResult() {
+		if (photonView.isMine) {
+			Roulette.RouletteAction currentAction = GameManager.instance.board.roulette.currentAction;
+			int fillCount = 0;
+			int emptyCount = 0;
+			switch (currentAction) {
+			case Roulette.RouletteAction.ONE_CHERRY:
+				fillCount = 1;
+				break;
+			case Roulette.RouletteAction.TWO_CHERRIES:
+				fillCount = 2;
+				break;
+			case Roulette.RouletteAction.THREE_CHERRIES:
+				fillCount = 3;
+				break;
+			case Roulette.RouletteAction.FOUR_CHERRIES:
+				fillCount = 4;
+				break;
+			case Roulette.RouletteAction.DOG:
+			case Roulette.RouletteAction.BIRD:
+				emptyCount = 2;
+				break;
+			case Roulette.RouletteAction.SPILLED_BASKET:
+				emptyCount = basket.cherries.Count;
+				break;
+			}
+			while (basket.cherries.Count > 0 && emptyCount > 0) {
+				emptyCount--;
+				Cherry item = basket.cherries [0];
+				basket.cherries.Remove (item);
+				tree.cherries.Add (item);
+				item.transform.parent = tree.transform;
+			}
+			while (tree.cherries.Count > 0 && fillCount > 0) {
+				fillCount--;
+				Cherry item = tree.cherries [0];
+				tree.cherries.Remove (item);
+				basket.cherries.Add (item);
+				item.transform.parent = basket.transform;
+			}
+			Debug.Log (playerName + " action is " + currentAction + " tree = " + tree.cherries.Count + " basket = " + basket.cherries.Count);
+			if (CheckForWinner ()) {
+				Debug.Log (playerName + " hi ho cherry o!!");
+			} else {
+				EndTurn ();
+			}
+		}
+	}
+
+	[PunRPC]
 	public void SpinRoulette() {
 		if (PhotonNetwork.isMasterClient) {
 			GameManager.instance.board.roulette.SpinRoullette ();
